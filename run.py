4b28799522ed03b8038f7f498668ec09afecb255
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-23 10:45:51 +0000

    Update run.py

--- run.py
+++ run.py
@@ -11,6 +11,6 @@ signal.signal(signal.SIGINT, signal_handler)
 
 if __name__ == "__main__":
     try:
-        uvicorn.run("app.main:app", host="0.0.0.0", port=80)
+        uvicorn.run("app.main:app", host="0.0.0.0", port=80, reload=True)
     except KeyboardInterrupt:
         logger.info('Server stopped by user (Ctrl+C)')
