32d4d6412fe6d4a8532ef044fc9a5f0d9fbdd1ee
Gabriel Roca <blast.blackstone@gmail.com>
2024-05-25 12:11:07 +0000

    Update routes.py
    - Elimados endpoints innecesarios.
    - Agregado endpoint '/get-k-linedata' para solicitar la informacion de las velas de un Ticker

--- app/bingx/routes.py
+++ app/bingx/routes.py
@@ -2,46 +2,14 @@
 # Description: Routes for BingX exchange
 
 from fastapi import APIRouter, Depends, HTTPException, Request
-from app.bingx.util import extract_order_variables
-from app.bingx.api import make_order, close_all_positions
+from app.bingx.api import get_k_line_data
 
 router = APIRouter()
 
-@router.route('/test-trade')
-def trade():
-    # Aquí puedes manejar los datos enviados en la solicitud si es necesario
-    
-    leverage = "50"
-    symbol = "BTC-USDT"
-    side = "BUY"
-    positionSide = "LONG"
-    order_type = "MARKET"
-    quantity = 0.0002
-    
-    result = make_order(leverage, symbol, side, positionSide, order_type, quantity)
-    print("----------------------------------------------------------------------------------------------------")
-    print(result)
-    print("----------------------------------------------------------------------------------------------------")
-    
-    variables = extract_order_variables(result)
-    
-    if not variables:
-        return {"error": "Datos no válidos o falta de variables"}
-
-    print("Variables extraídas:")
-    for key, value in variables.items():
-        print(f"{key}: {value}")        
-    
-    return ({"status": "order executed", "result": result})
-
-@router.route('/test-close-all')
-def close_all():
-    symbol = "BTC-USDT"
-    
-    result = close_all_positions(symbol)
-    print("----------------------------------------------------------------------------------------------------")
-    print(result)
-    print("----------------------------------------------------------------------------------------------------")       
-    
-    return ({"status": "all positions closed", "result": result})
-
+@router.get('/get-k-line-data')
+async def get_k_line_data_endpoint(symbol: str, interval: str, limit: str, start_date: str, end_date: str):
+    try:       
+        data = await get_k_line_data(symbol, interval, limit, start_date, end_date)
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
