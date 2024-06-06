5033336318cd1dac49df3115691a43fcedc2df14
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-06 20:58:51 +0000

    Update routes.py
    Implementacion seguridad por IP en el endpoint '/bingx/get-k-line-data'

--- app/bingx/routes.py
+++ app/bingx/routes.py
@@ -3,11 +3,20 @@
 
 from fastapi import APIRouter, Depends, HTTPException, Request
 from app.bingx.api import get_k_line_data
+from loguru import logger
+from app.utils.ip_check import is_ip_allowed
 
 router = APIRouter()
 
 @router.get('/get-k-line-data')
-async def get_k_line_data_endpoint(symbol: str, interval: str, limit: str, start_date: str, end_date: str):
+async def get_k_line_data_endpoint(request: Request, symbol: str, interval: str, limit: str, start_date: str, end_date: str):
+    client_ip = request.client.host
+    
+    logger.info(f"Fetching K-Line data for {symbol} from {client_ip}")
+
+    # Verificar si la IP está permitida
+    await is_ip_allowed(client_ip)
+    
     try:       
         data = await get_k_line_data(symbol, interval, limit, start_date, end_date)
         return data
