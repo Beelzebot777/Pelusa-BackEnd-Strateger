75fd2381c43cfc438a3439c182d9cac6f32a8944
Gabriel Roca <blast.blackstone@gmail.com>
2024-06-07 11:48:56 +0000

    Update routes.py
    Implementacion Verificacion de ip permitida.

--- app/strateger/routes.py
+++ app/strateger/routes.py
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 from app.siteground.database import get_db_ordenes
 from app.strateger.schemas import OrderResponse
 from app.strateger.crud import get_orders
+from app.utils.ip_check import is_ip_allowed
 
 from loguru import logger
 from typing import List
@@ -13,7 +14,12 @@ from typing import List
 router = APIRouter()
 
 @router.get("/orders", response_model=List[OrderResponse])
-async def get_orders_endpoint(db_orders: Session = Depends(get_db_ordenes)):
+async def get_orders_endpoint(request: Request, db_orders: Session = Depends(get_db_ordenes)):
+    client_ip = request.client.host
+    # Verificar si la IP está permitida
+    
+    await is_ip_allowed(client_ip)
+    
     try:
         orders = get_orders(db_orders)
         return [OrderResponse.model_validate(order) for order in orders]
