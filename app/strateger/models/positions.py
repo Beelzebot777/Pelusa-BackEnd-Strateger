7846ed56066c6ca2048723ad2d2aef87f5789016
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-14 18:51:19 +0000

    feat: el models de 'positions' ahora guarda coherencia con las columnas que pueden ser o no nulas

--- app/strateger/models/positions.py
+++ app/strateger/models/positions.py
@@ -9,22 +9,22 @@ class Position(BasePositions):
     id = Column(Integer, primary_key=True, index=True)
     account_name = Column(String(50), nullable=False)  # Puede ser: Main, Subaccount, etc.
     account_type = Column(String(50), nullable=False)  # Del tipo: 'Perp USDT-M', 'Perp COIN-M', 'Spot'
-    symbol = Column(String(50), nullable=False)
-    positionId = Column(String(50), nullable=False)
-    positionSide = Column(String(50), nullable=False)
-    isolated = Column(Boolean, nullable=False)
-    positionAmt = Column(String(50), nullable=False)
-    availableAmt = Column(String(50), nullable=False)
-    unrealizedProfit = Column(String(50), nullable=False)
+    symbol = Column(String(50), nullable=True)
+    positionId = Column(String(50), nullable=True)
+    positionSide = Column(String(50), nullable=True)
+    isolated = Column(Boolean, nullable=True)
+    positionAmt = Column(String(50), nullable=True)
+    availableAmt = Column(String(50), nullable=True)
+    unrealizedProfit = Column(String(50), nullable=True)
     realisedProfit = Column(String(50), nullable=True)  # Campo adicional
-    initialMargin = Column(String(50), nullable=False)
+    initialMargin = Column(String(50), nullable=True)
     margin = Column(String(50), nullable=True)  # Campo adicional
-    avgPrice = Column(String(50), nullable=False)
-    liquidationPrice = Column(Float, nullable=False)
-    leverage = Column(Integer, nullable=False)
+    avgPrice = Column(String(50), nullable=True)
+    liquidationPrice = Column(Float, nullable=True)
+    leverage = Column(Integer, nullable=True)
     positionValue = Column(String(50), nullable=True)  # Campo adicional
-    markPrice = Column(String(50), nullable=False)
-    riskRate = Column(String(50), nullable=False)
-    maxMarginReduction = Column(String(50), nullable=False)
+    markPrice = Column(String(50), nullable=True)
+    riskRate = Column(String(50), nullable=True)
+    maxMarginReduction = Column(String(50), nullable=True)
     pnlRatio = Column(String(50), nullable=True)  # Campo adicional
-    updateTime = Column(BigInteger, nullable=False)
+    updateTime = Column(BigInteger, nullable=True)
