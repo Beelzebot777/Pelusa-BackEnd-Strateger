bb0349b9af9fda0da715f5bc57e269a77cc5af4c
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-23 20:44:35 +0000

    Refactor: En 'strateger' ---> 'accounts'
    get_all_accounts ahora es get_all_data_bingx_accounts

--- app/strateger/routes/accounts.py
+++ app/strateger/routes/accounts.py
@@ -5,12 +5,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import SQLAlchemyError
 from app.siteground.database import get_db_accounts
 from app.strateger.schemas.accounts import AccountListResponse
-from app.strateger.crud.accounts import get_all_accounts
+from app.strateger.crud.accounts import get_all_data_bingx_accounts
 from datetime import datetime
 
 router = APIRouter()
 
-@router.get("/get-all-data", response_model=AccountListResponse)
+@router.get("/get-all-data-bingx-acc", response_model=AccountListResponse)
 async def get_all_accounts_endpoint(db: AsyncSession = Depends(get_db_accounts)):
     """
     Retrieve all accounts from the database.
@@ -25,7 +25,7 @@ async def get_all_accounts_endpoint(db: AsyncSession = Depends(get_db_accounts))
         HTTPException: If there is an error retrieving the accounts from the database.
     """
     try:
-        accounts = await get_all_accounts(db)
+        accounts = await get_all_data_bingx_accounts(db)
     except SQLAlchemyError as e:
         raise HTTPException(status_code=400, detail=str(e))
     return AccountListResponse(code=200, msg="Success", timestamp=int(datetime.utcnow().timestamp()), data=accounts)
