7def56c147f7ed569f03ee93940e43751b2301dd
Gabriel Roca <blast.blackstone@gmail.com>
2024-07-23 20:44:42 +0000

    Update accounts.py

--- app/strateger/crud/accounts.py
+++ app/strateger/crud/accounts.py
@@ -4,8 +4,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.exc import SQLAlchemyError
 from app.strateger.models.accounts import Account
+from fastapi import HTTPException
 
-async def get_all_accounts(db: AsyncSession):
+async def get_all_data_bingx_accounts(db: AsyncSession):
     try:
         result = await db.execute(select(Account))
         return result.scalars().all()
