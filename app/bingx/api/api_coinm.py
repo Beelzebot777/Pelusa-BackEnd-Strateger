fcdd77d97d74d1dfde29d703ed8921722aa58f00
Gabriel Roca <blast.blackstone@gmail.com>
2024-10-17 22:21:51 +0000

    Chore(api_coinm.py): Added endpoint API from bingx for coinm

--- app/bingx/api/api_coinm.py
+++ app/bingx/api/api_coinm.py
@@ -1,10 +1,13 @@
-# Description: COIN-M Perp Futures functions for BingX exchange
+# Path: app/bingx/api/api_coinm.py
+
+# Description: Coin-M Perp Futures functions for BingX exchange
 import time
 from .api_utils import send_request, parse_param
-from loguru import logger
 
+#!--------------------------------------  Coin-M Trade Endpoints  ---------------------------------------!#
+
+#TODO 1. Place Order
 async def make_order_coinm(leverage, symbol, side, positionSide, order_type, quantity):
-
     payload = {}
     path = '/openApi/cswap/v1/trade/order'
     method = "POST"
@@ -20,6 +23,65 @@ async def make_order_coinm(leverage, symbol, side, positionSide, order_type, qua
     paramsStr = parse_param(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
+#TODO 2. Query Commission Rate
+async def query_commission_rate():
+    """
+    Queries the commission rate for the current user in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/user/commissionRate'
+    method = "GET"
+    paramsMap = {
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 3. Query Leverage
+async def query_leverage(symbol):
+    """
+    Queries the leverage for the specified symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/leverage'
+    method = "GET"
+    paramsMap = {
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+
+#TODO 4. Modify Leverage
+async def modify_leverage(symbol, side, leverage):
+    """
+    Modifies the leverage for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/leverage'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "side": side,
+        "leverage": leverage,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 5. Cancel all orders
+async def cancel_all_open_orders(symbol):
+    """
+    Cancels all open orders for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/allOpenOrders'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 6. Close All Positions in Bulk
 async def close_all_positions_coinm(symbol):
     payload = {}
     path = '/openApi/cswap/v1/trade/closeAllPositions'
@@ -31,76 +93,20 @@ async def close_all_positions_coinm(symbol):
     paramsStr = parse_param(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
-async def get_balance_perp_coinm():
-    """
-    Envia una solicitud asíncrona para obtener el balance de cuenta en el mercado perpetuo de contratos coin-margined (COIN-M).
-
-    Parámetros de la Solicitud:
-    --------------------------
-    La solicitud utiliza el método GET y envía los siguientes parámetros de consulta:
-
-    - symbol (string, opcional): Par de trading, por ejemplo, BTC-USD, usar letras mayúsculas.
-    - timestamp (int64, requerido): Marca de tiempo de la solicitud, Unidad: milisegundos.
-    - recvWindow (int64, opcional): Valor de ventana de tiempo válido de la solicitud, Unidad: milisegundos.
-
-    Cuerpo de la Solicitud:
-    -----------------------
-    El cuerpo de la solicitud está vacío ({}).
-
-    Endpoint:
-    ---------
-    /openApi/cswap/v1/user/balance
-
-    Respuesta:
-    ----------
-    La respuesta contiene los siguientes campos:
-
-    - code (int32): Código de estado.
-    - msg (string): Mensaje de descripción.
-    - timestamp (int64): Marca de tiempo de generación de la respuesta, Unidad: milisegundos.
-    - data (List[Data]): Lista de activos con los siguientes campos:
-
-        - asset (string): Activo del usuario.
-        - balance (string): Balance del activo.
-        - equity (string): Valor neto del activo.
-        - unrealizedProfit (string): Ganancia no realizada.
-        - availableMargin (string): Margen disponible.
-        - usedMargin (string): Margen usado.
-        - freezedMargin (string): Margen congelado.
-        - shortUid (string): UID del usuario.
-
-    Ejemplo de Respuesta:
-    ---------------------
-    {
-        "code": 200,
-        "msg": "Success",
-        "timestamp": 1627891234567,
-        "data": [
-            {
-                "asset": "BTC",
-                "balance": "0.500",
-                "equity": "0.505",
-                "unrealizedProfit": "0.005",
-                "availableMargin": "0.400",
-                "usedMargin": "0.100",
-                "freezedMargin": "0.000",
-                "shortUid": "123456789"
-            }
-        ]
+#TODO 7. Query warehouse
+async def get_positions_perp_coinm():   
+    payload = {}
+    path = '/openApi/cswap/v1/user/positions'
+    method = "GET"
+    paramsMap = {
+        "timestamp": str(int(time.time() * 1000)),
     }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, payload)
 
-    Códigos de Estado de Respuesta:
-    -------------------------------
-    - 200 OK: La solicitud se ha procesado correctamente.
-    - 400 Bad Request: La solicitud contiene un error (por ejemplo, timestamp no válido).
-    - 401 Unauthorized: La solicitud no está autorizada.
-    - 500 Internal Server Error: Error del servidor.
 
-    Notas Adicionales:
-    ------------------
-    - Asegúrate de que la marca de tiempo (timestamp) sea válida y esté en el rango permitido.
-    - La función send_request debe manejar la firma y el envío de la solicitud al servidor.
-    """
+#TODO 8. Query Account Assets
+async def get_balance_perp_coinm():    
     payload = {}
     path = '/openApi/cswap/v1/user/balance'
     method = "GET"
@@ -110,43 +116,145 @@ async def get_balance_perp_coinm():
     paramsStr = parse_param(paramsMap)
     return send_request(method, path, paramsStr, payload)
 
-async def get_positions_perp_coinm():
+#TODO 9. Query Force Orders
+async def query_force_orders(symbol=None, startTime=None, endTime=None, limit=100):
     """
-    Fetches the user's positions for COIN-M perpetual futures.
-
-    Request Parameters:
-    - symbol (string, optional): Trading pair, e.g., BTC-USD. Use uppercase letters.
-    - timestamp (int64, required): Request time stamp in milliseconds.
-    - recvWindow (int64, optional): Request valid time window value in milliseconds.
-
-    Response:
-    - code (int32): Status code.
-    - msg (string): Description information.
-    - timestamp (int64): Response generation time point in milliseconds.
-    - data (List[Data]): List of positions.
-
-    Data Fields:
-    - symbol (string): Trading pair.
-    - positionId (string): Position number.
-    - positionSide (string): Holding direction, bi-directional position can only be LONG or SHORT.
-    - isolated (bool): Indicates if it is per position mode. True: per position mode, False: full position.
-    - positionAmt (string): Holding quantity.
-    - availableAmt (string): Quantity that can be closed.
-    - unrealizedProfit (string): Unrealized profit.
-    - initialMargin (string): Initial margin.
-    - liquidationPrice (float64): Force liquidation price.
-    - avgPrice (string): Opening average price.
-    - leverage (int32): Leverage.
-    - markPrice (string): Mark price.
-    - riskRate (string): Risk rate.
-    - maxMarginReduction (string): Maximum reduction of margin.
-    - updateTime (int64): Position update time in milliseconds.
+    Queries forced liquidation orders in Coin-M futures.
     """
-    payload = {}
-    path = '/openApi/cswap/v1/user/positions'
+    path = '/openApi/cswap/v1/trade/forceOrders'
     method = "GET"
     paramsMap = {
-        "timestamp": str(int(time.time() * 1000)),
+        "symbol": symbol,
+        "startTime": startTime,
+        "endTime": endTime,
+        "limit": str(limit),
+        "timestamp": str(int(time.time() * 1000))
     }
     paramsStr = parse_param(paramsMap)
-    return send_request(method, path, paramsStr, payload)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 10. Query Order Trade Detail
+async def query_historical_orders(symbol, startTime, endTime, limit=100):
+    """
+    Queries historical transaction orders for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/allFillOrders'
+    method = "GET"
+    paramsMap = {
+        "symbol": symbol,
+        "startTime": startTime,
+        "endTime": endTime,
+        "limit": str(limit),
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 11. Cancel an Order
+async def cancel_order(orderId, symbol):
+    """
+    Cancels a specific order in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/cancelOrder'
+    method = "DELETE"
+    paramsMap = {
+        "orderId": str(orderId),
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 12. Query all current pending orders
+async def query_all_open_orders(symbol):
+    """
+    Queries all open orders for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/openOrders'
+    method = "GET"
+    paramsMap = {
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 13. Query Order
+async def query_order(orderId, symbol):
+    """
+    Queries a specific order by order ID in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/orderDetail'
+    method = "GET"
+    paramsMap = {
+        "orderId": str(orderId),
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 14. Query User's History Orders
+async def query_history_orders(symbol, startTime, endTime, limit=500):
+    """
+    Queries the user's historical orders in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/orderHistory'
+    method = "GET"
+    paramsMap = {
+        "symbol": symbol,
+        "startTime": startTime,
+        "endTime": endTime,
+        "limit": str(limit),
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+
+#TODO 15. Query Margin Type
+async def query_margin_type(symbol):
+    """
+    Queries the margin type for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/marginType'
+    method = "GET"
+    paramsMap = {
+        "symbol": symbol,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 16. Set Margin Type
+async def set_margin_type(symbol, marginType):
+    """
+    Sets the margin type for a specific symbol in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/marginType'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "marginType": marginType,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
+#TODO 17. Adjust Isolated Margin
+async def adjust_isolated_position_margin(symbol, amount, positionSide, type):
+    """
+    Modifies the isolated margin for a position in Coin-M futures.
+    """
+    path = '/openApi/cswap/v1/trade/positionMargin'
+    method = "POST"
+    paramsMap = {
+        "symbol": symbol,
+        "amount": str(amount),
+        "positionSide": positionSide,
+        "type": type,
+        "timestamp": str(int(time.time() * 1000))
+    }
+    paramsStr = parse_param(paramsMap)
+    return send_request(method, path, paramsStr, {})
+
