c927134ee8bf6ffb713da64de4c1eb282eeadb73
Gabriel Roca <blast.blackstone@gmail.com>
2024-11-18 10:55:01 +0000

    Chore(alarms): Added docs for endpoint '/webhook'

--- app/alarms/routes.py
+++ app/alarms/routes.py
@@ -16,6 +16,18 @@ from app.strateger.utils.orders import crear_operacion                      #Ope
 
 router = APIRouter()
 
+"""
+    Handle incoming webhook requests to create and process alarms.
+    Args:
+        request (Request): The incoming HTTP request.
+        alarm_data (AlarmCreate): The alarm data received in the request body.
+        db_alarmas (AsyncSession): Database session for alarm operations.
+        db_estrategias (AsyncSession): Database session for strategy operations.
+    Returns:
+        AlarmResponse: The response model containing the saved alarm details.
+    Raises:
+        HTTPException: If the IP is not allowed or any other error occurs during processing.
+"""
 @router.post("/webhook", response_model=AlarmResponse)
 async def webhook(request: Request, alarm_data: AlarmCreate, db_alarmas: AsyncSession = Depends(get_db_alarmas), db_estrategias: AsyncSession = Depends(get_db_estrategias)):
     try:
